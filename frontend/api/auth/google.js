1fe0774e1489225b19adfa8456f13cff1d7103ca
Aldwyn Bernardo <frankiexander34@gmail.com>
2026-06-23 10:38:11 +0800

    auth fixes 4

--- frontend/api/auth/google.js
+++ frontend/api/auth/google.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       client_id: process.env.GOOGLE_CLIENT_ID,
       access_type: "offline",
       response_type: "code",
-      prompt: "consent",
+      prompt: "none",
       scope: [
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
